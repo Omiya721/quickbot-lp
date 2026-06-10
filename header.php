88f357bf056b21b3d681115c877dac584be6a615
Omiya721 <m.omiya0614@gmail.com>
2026-06-10 19:28:43 +0900

    [修正]サイトタイトルを固定ページのタイトルに

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
-        <title>QuickBot. | 24時間AI自動応答の次世代チャットボット</title>
         <?php wp_head(); ?>
     </head>
 
